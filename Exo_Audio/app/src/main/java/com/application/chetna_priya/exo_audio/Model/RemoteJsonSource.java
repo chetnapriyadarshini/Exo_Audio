cddf9fdf179392f9f702c38928e8fdd44c634979
chetna <chetna.priya@gmail.com>
2016-11-25 22:07:40 -0700

    feature: add proper parsing for time duration in diff formats

--- Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Model/RemoteJsonSource.java
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Model/RemoteJsonSource.java
@@ -17,20 +17,6 @@ import static android.content.ContentValues.TAG;
 
 public class RemoteJsonSource implements MediaProviderSource {
 
-    private static final String JSON_PODCAST = "podcast";
-    //private static final String JSON_TITLE = "title";//check
-    private static final String JSON_ALBUM = "album";//
-    private static final String JSON_ARTIST = "artist";//check
-    private static final String JSON_GENRE = "genre";//check
-    private static final String JSON_SOURCE = "source";//check
-    private static final String JSON_IMAGE = "image";//check
-    private static final String JSON_TRACK_NUMBER = "trackNumber";//check
-    private static final String JSON_TOTAL_TRACK_COUNT = "totalTrackCount";//check
-    private static final String JSON_TRACK_URI = "track_uri";
-   // private static final String JSON_DURATION = "duration";
-
-
-
     @Override
     public Iterator<MediaMetadataCompat> iterator() {
         try {
@@ -54,7 +40,8 @@ public class RemoteJsonSource implements MediaProviderSource {
         // the session metadata can be accessed by notification listeners. This is done in this
         // sample for convenience only.
         //noinspection ResourceType
-        long duration = /*Long.parseLong(episode.getEpisode_duration())**/1000;//ms
+        long duration = convertStringDurationToMs(episode.getEpisode_duration());
+        ///*Long.parseLong(episode.getEpisode_duration())**/1000;//ms
         return new MediaMetadataCompat.Builder()
                 .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, episode.getId())
                 .putString(MediaProviderSource.CUSTOM_METADATA_TRACK_SOURCE, episode.getEpisode_pod_link())
@@ -67,6 +54,52 @@ public class RemoteJsonSource implements MediaProviderSource {
                 .putLong(MediaMetadataCompat.METADATA_KEY_TRACK_NUMBER, episode.getPodcast().getTrackNumber())
                 .putLong(MediaMetadataCompat.METADATA_KEY_NUM_TRACKS, episode.getPodcast().getTotalTrackCount())
                 .build();
+    }
+
+    private long convertStringDurationToMs(String episode_duration) {
+        Log.d(TAG, "RECEIVEDDDDDD episode_duration for parsingggggggg "+episode_duration);
+
+        /*
+        The tag can be formatted HH:MM:SS, H:MM:SS, MM:SS, or M:SS (H = hours, M = minutes, S = seconds).
+        If an integer is provided (no colon present), the value is assumed to be in seconds.
+        If one colon is present, the number to the left is assumed to be minutes,
+        and the number to the right is assumed to be seconds.
+        If more than two colons are present, the numbers furthest to the right are ignored.
+         */
+
+        if(!episode_duration.contains(":")){
+            /*
+            Accounts for condition: If an integer is provided (no colon present), the value is assumed to be in seconds.
+             */
+            return Long.parseLong(episode_duration)*1000;
+        }
+
+        long duration = 0;
+        String[] durArr = episode_duration.split(":");
+
+        long hr =  0;
+        long min = 0;
+        long sec = 0;
+        int currIndex = 0;
+
+        if(durArr.length == 3) {
+            hr = Long.parseLong(durArr[currIndex]) * 60 * 60 * 1000;//in msecs
+            currIndex++;
+        }
+
+
+        if(durArr.length >= 2) {
+            min = Long.parseLong(durArr[currIndex]) * 60 * 1000;//in msecs
+            currIndex++;
+        }
+
+        sec = Long.parseLong(durArr[currIndex])*1000;//in msecs
+
+        duration = hr + min + sec;
+
+        Log.d(TAG, "Returning durationnnnnnnnnn "+duration);
+
+        return duration;
     }
 
 }
