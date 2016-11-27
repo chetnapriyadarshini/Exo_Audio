90144b96547e58c12fb5b68f6973731c49d28b12
chetna <chetna.priya@gmail.com>
2016-11-27 09:57:25 -0700

    refactor: Add class for the drawer activity, this class will serve as the base activity class for other activities

---
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/DrawerActivity.java
@@ -0,0 +1,15 @@
+package com.application.chetna_priya.exo_audio.Ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.application.chetna_priya.exo_audio.R;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
