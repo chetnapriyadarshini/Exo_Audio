7468c8fe9b25ca0d336dd32e1007249125ca6444
chetna <chetna.priya@gmail.com>
2016-11-27 11:15:41 -0700

    refactor : Add components for drawer and drawer interaction

--- Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/DrawerActivity.java
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/DrawerActivity.java
@@ -1,12 +1,17 @@
 package com.application.chetna_priya.exo_audio.Ui;
 
 import android.app.ActivityOptions;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.application.chetna_priya.exo_audio.R;
@@ -68,5 +73,146 @@ public class DrawerActivity extends AppCompatActivity {
                     .setTitle(R.string.app_name);
         }
     };
+
+    private final FragmentManager.OnBackStackChangedListener mBackStackChangedListener =
+            new FragmentManager.OnBackStackChangedListener() {
+                @Override
+                public void onBackStackChanged() {
+                    updateDrawerToggle();
+                }
+            };
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if (mDrawerToggle != null) {
+            mDrawerToggle.syncState();
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Whenever the fragment back stack changes, we may need to update the
+        // action bar toggle: only top level screens show the hamburger-like icon, inner
+        // screens - either Activities or fragments - show the "Up" icon instead.
+        getFragmentManager().addOnBackStackChangedListener(mBackStackChangedListener);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (mDrawerToggle != null) {
+            mDrawerToggle.onConfigurationChanged(newConfig);
+        }
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getFragmentManager().removeOnBackStackChangedListener(mBackStackChangedListener);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        // If the drawer is open, back will close it
+        if (mDrawerLayout != null && mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawers();
+            return;
+        }
+        // Otherwise, it may return to the previous fragment stack
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager.getBackStackEntryCount() > 0) {
+            fragmentManager.popBackStack();
+        } else {
+            // Lastly, it will rely on the system behavior for back
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+        mToolbar.setTitle(title);
+    }
+
+    @Override
+    public void setTitle(int titleId) {
+        super.setTitle(titleId);
+        mToolbar.setTitle(titleId);
+    }
+
+    protected void initializeToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar == null) {
+            throw new IllegalStateException("Layout is required to include a Toolbar with id " +
+                    "'toolbar'");
+        }
+        mToolbar.inflateMenu(R.menu.main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (mDrawerLayout != null) {
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            if (navigationView == null) {
+                throw new IllegalStateException("Layout requires a NavigationView " +
+                        "with id 'nav_view'");
+            }
+
+            // Create an ActionBarDrawerToggle that will handle opening/closing of the drawer:
+            mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                    mToolbar, R.string.open_content_drawer, R.string.close_content_drawer);
+            mDrawerLayout.setDrawerListener(mDrawerListener);
+            populateDrawerItems(navigationView);
+            setSupportActionBar(mToolbar);
+            updateDrawerToggle();
+        } else {
+            setSupportActionBar(mToolbar);
+        }
+
+        mToolbarInitialized = true;
+    }
+
+
+    private void populateDrawerItems(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mItemToOpenWhenDrawerCloses = menuItem.getItemId();
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+        //TODO Implementation of navigation view
+        if (DrawerActivity.class.isAssignableFrom(getClass())) {
+         //   navigationView.setCheckedItem(R.id.navigation_allmusic);
+        } //else if (PlaceholderActivity.class.isAssignableFrom(getClass()))
+        {
+         //   navigationView.setCheckedItem(R.id.navigation_playlists);
+        }
+    }
+
+    protected void updateDrawerToggle() {
+        if (mDrawerToggle == null) {
+            return;
+        }
+        boolean isRoot = getFragmentManager().getBackStackEntryCount() == 0;
+        mDrawerToggle.setDrawerIndicatorEnabled(isRoot);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(!isRoot);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(!isRoot);
+            getSupportActionBar().setHomeButtonEnabled(!isRoot);
+        }
+        if (isRoot) {
+            mDrawerToggle.syncState();
+        }
+    }
 
 }
