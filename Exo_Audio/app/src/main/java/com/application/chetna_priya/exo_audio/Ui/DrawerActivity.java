76fa4171d778b220c0e1d9e7da824eb32a9e8f33
chetna <chetna.priya@gmail.com>
2016-11-27 10:33:59 -0700

    refactor : Add drawer listener to DrawerActivity and fade_in fade_out animations, drawer.xml menu layout

--- Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/DrawerActivity.java
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/DrawerActivity.java
@@ -1,15 +1,72 @@
 package com.application.chetna_priya.exo_audio.Ui;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.application.chetna_priya.exo_audio.R;
 
 public class DrawerActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer);
-    }
+    private static final String TAG = DrawerActivity.class.getSimpleName();
+
+    private static final int DELAY_MILLIS = 1000;
+
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private boolean mToolbarInitialized;
+
+    private int mItemToOpenWhenDrawerCloses = -1;
+
+
+    private final DrawerLayout.DrawerListener mDrawerListener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            if (mDrawerToggle != null) mDrawerToggle.onDrawerClosed(drawerView);
+            if (mItemToOpenWhenDrawerCloses >= 0) {
+                Bundle extras = ActivityOptions.makeCustomAnimation(
+                        DrawerActivity.this, R.anim.fade_in, R.anim.fade_out).toBundle();
+
+                Class activityClass = null;
+                switch (mItemToOpenWhenDrawerCloses) {
+                    //TODO Add items and activity classes later
+                    //case R.id.navigation_allmusic:
+                      //  activityClass = MusicPlayerActivity.class;
+                      //  break;
+                    //case R.id.navigation_playlists:
+                      //  activityClass = PlaceholderActivity.class;
+                        //break;
+                }
+                if (activityClass != null) {
+                    startActivity(new Intent(DrawerActivity.this, activityClass), extras);
+                    finish();
+                }
+            }
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+            if (mDrawerToggle != null) mDrawerToggle.onDrawerStateChanged(newState);
+        }
+
+        @Override
+        public void onDrawerSlide(View drawerView, float slideOffset) {
+            if (mDrawerToggle != null) mDrawerToggle.onDrawerSlide(drawerView, slideOffset);
+        }
+
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            if (mDrawerToggle != null) mDrawerToggle.onDrawerOpened(drawerView);
+            if (getSupportActionBar() != null) getSupportActionBar()
+                    .setTitle(R.string.app_name);
+        }
+    };
+
 }
