9dac87c506b962faf92b5fe36aa15bc58496f5c3
chetna <chetna.priya@gmail.com>
2016-11-29 15:33:51 -0500

    refactor: Add a playbackControls for showing a small audio view whenever an audio is playing

--- Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/PlaybackControlsFragment.java
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/PlaybackControlsFragment.java
@@ -1,14 +1,20 @@
 package com.application.chetna_priya.exo_audio.Ui;
 
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.application.chetna_priya.exo_audio.R;
+
+import static android.content.ContentValues.TAG;
 
 public class PlaybackControlsFragment extends Fragment {
 
@@ -18,12 +24,32 @@ public class PlaybackControlsFragment extends Fragment {
     }
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
-    }
+    // Receive callbacks from the MediaController. Here we update our state such as which queue
+    // is being shown, the current title and description and the PlaybackState.
+    private final MediaControllerCompat.Callback mCallback = new MediaControllerCompat.Callback() {
+        @Override
+        public void onPlaybackStateChanged(@NonNull PlaybackStateCompat state) {
+            Log.d(TAG, "Received playback state change to state "+ state.getState());
+            PlaybackControlsFragment.this.onPlaybackStateChanged(state);
+        }
 
+        @Override
+        public void onMetadataChanged(MediaMetadataCompat metadata) {
+            if (metadata == null) {
+                return;
+            }
+            Log.d(TAG, "Received metadata state change to mediaId="+
+                    metadata.getDescription().getMediaId()+
+                    " song="+ metadata.getDescription().getTitle());
+            PlaybackControlsFragment.this.onMetadataChanged(metadata);
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_playback_controls, container, false);
+
+        return rootView;
+    }
 }
