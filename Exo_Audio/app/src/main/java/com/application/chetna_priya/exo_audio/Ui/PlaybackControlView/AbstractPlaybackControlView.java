6df6bba66a22e6a1af54755323c458fd8199efe9
chetna <chetna.priya@gmail.com>
2016-12-01 12:52:52 -0500

    refactor : Comment out activity callbacks interface

--- Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/PlaybackControlView/AbstractPlaybackControlView.java
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/PlaybackControlView/AbstractPlaybackControlView.java
@@ -16,35 +16,38 @@ public abstract class AbstractPlaybackControlView extends FrameLayout /*implemen
 
 
     //private Context mContext;
-    ActivityCallbacks activityCallbacks;
+   // ActivityCallbacks activityCallbacks;
+
+    /*
     public static final String EVENT_POSITION_DISCONTINUITY = "event_position_discontinuity";
     public static final String EVENT_SPEED_CHANGE = "event_speed_change";
     public static final String EVENT_TIME_LINE_CHANGED = "event_timeline_changed";
-    public static final String EVENT_PLAYER_CHANGED = "event_player_changed";
+    public static final String EVENT_PLAYER_CHANGED = "event_player_changed";*/
     public static final String CUSTOM_ACTION_SPEED_CHANGE = "custom_action_speed_changed";
     public static final String SPEED = "speed";
 
    // public abstract void disconnectSession();
 
-    public interface ActivityCallbacks {
-        void finishActivity();
-        void setSupportMediaControllerForActivity(MediaControllerCompat mediaController);
+    /*public interface ActivityCallbacks {
+     //   void finishActivity();
+      //  void setSupportMediaControllerForActivity(MediaControllerCompat mediaController);
         //void setMediaBrowser(MediaBrowserCompat mediaBrowser);
-    }
+    }*/
 
 
     public AbstractPlaybackControlView(Context context) {
-        super(context);activityCallbacks = (ActivityCallbacks) context;
+        super(context);
+        //activityCallbacks = (ActivityCallbacks) context;
     }
 
     public AbstractPlaybackControlView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        activityCallbacks = (ActivityCallbacks) context;
+       // activityCallbacks = (ActivityCallbacks) context;
     }
 
 
     public AbstractPlaybackControlView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        activityCallbacks = (ActivityCallbacks) context;
+     //   activityCallbacks = (ActivityCallbacks) context;
     }
 }
