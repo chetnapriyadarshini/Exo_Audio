af9c0dd96a0bdb1e59c0766ee05af8513f242993
chetna <chetna.priya@gmail.com>
2016-12-05 16:03:18 -0700

    fix : Preference available across all activities, not returning null now on being read from MainActivity and saved in GenreActivity
    Add a boolean preference to check is_genre_preference_set

--- Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Utils/PreferenceHelper.java
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Utils/PreferenceHelper.java
@@ -1,14 +1,38 @@
 package com.application.chetna_priya.exo_audio.Utils;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-import com.application.chetna_priya.exo_audio.Ui.MainActivity;
+import java.util.ArrayList;
+import java.util.HashSet;
 
-/**
- * Created by chetna_priya on 12/1/2016.
- */
 public class PreferenceHelper {
+    private static final String TAG = PreferenceHelper.class.getSimpleName();
+    private static final String SAVED_GENRES = "saved_genres";
+    private static final String IS_GENRE_PREFERENCE_SET = "is_genre_preference_set";
+
     public static boolean isInitialGenrePreferenceSet(Context context) {
-        return false;
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPref.getBoolean(IS_GENRE_PREFERENCE_SET, false);
+    }
+
+    public static void saveGenrePreferences(Context context, ArrayList<String> savedGenres) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putStringSet(SAVED_GENRES, new HashSet<>(savedGenres));
+        editor.putBoolean(IS_GENRE_PREFERENCE_SET, true);
+        editor.apply();
+    }
+
+    public static ArrayList<String> getSavedGenres(Context context) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        HashSet<String> set = (HashSet<String>) sharedPref.getStringSet(SAVED_GENRES, null);
+        if(set == null) {
+            return null;
+        }
+
+        return new ArrayList<>(set);
     }
 }
