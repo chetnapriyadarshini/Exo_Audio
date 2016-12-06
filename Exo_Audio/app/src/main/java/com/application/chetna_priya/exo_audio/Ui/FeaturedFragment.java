7a3c12671c28ef1872a68d3cd349394647574371
chetna <chetna.priya@gmail.com>
2016-12-06 13:53:23 -0700

    refactor: Start with featured fragment
    1)Introduce layout and list items

--- Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/FeaturedFragment.java
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/FeaturedFragment.java
@@ -1,9 +1,24 @@
 package com.application.chetna_priya.exo_audio.Ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-/**
- * Created by chetna_priya on 12/6/2016.
- */
+import com.application.chetna_priya.exo_audio.R;
+
 public class FeaturedFragment extends Fragment {
+    /*
+    public static FeaturedFragment getInstance(String category_title, String category_url){
+
+    }*/
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.layout_featured_fragment, container, false);
+        return rootView;
+    }
 }
