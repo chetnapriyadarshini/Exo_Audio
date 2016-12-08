0fd9b6137470f2a6ff03be47f058dce0eb3368b8
chetna <chetna.priya@gmail.com>
2016-12-08 16:11:41 -0700

    refactor: Initial commit, manifest additon, list tiem created

---
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/AllPodcastsInCategory.java
@@ -0,0 +1,11 @@
+package com.application.chetna_priya.exo_audio.Ui;
+
+import android.os.Bundle;
+
+class AllPodcastsInCategory extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
