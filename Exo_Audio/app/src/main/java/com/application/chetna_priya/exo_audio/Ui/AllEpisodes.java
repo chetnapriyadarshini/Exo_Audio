1d53e01658001ea830f4c8419cd4448d2bb7a689
chetna <chetna.priya@gmail.com>
2016-12-30 22:40:40 -0700

    fix : String format in Allepisodes

--- Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/AllEpisodes.java
+++ Exo_Audio/app/src/main/java/com/application/chetna_priya/exo_audio/Ui/AllEpisodes.java
@@ -190,7 +190,7 @@ public class AllEpisodes extends BaseActivity {
             releaseDate = releaseDate.substring(0,releaseDate.length()-6);
             if(release_year == year){
                 //5 - 4 for year eg 1999 and 1 for space
-                releaseDate = (releaseDate.replace(" ", "%n"));
+                releaseDate = String.format(releaseDate.replace(" ", "%n"));
             }else {
                 releaseDate = releaseDate.concat("\n"+release_year);
             }
